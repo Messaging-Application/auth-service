662259b036bf21ef42e522f56b8c2e34d2c11b70
tomascandeias <tomaslcandeias@outlook.com>
2024-02-18 16:48:24 +0100

    login service and endpoint

--- src/main/java/ma/messaging/usermanagementservice/service/AccountService.java
+++ src/main/java/ma/messaging/usermanagementservice/service/AccountService.java
@@ -1,7 +1,10 @@
 package ma.messaging.usermanagementservice.service;
 
+import ma.messaging.usermanagementservice.user.login.LoginRequest;
+import ma.messaging.usermanagementservice.user.login.LoginResponse;
 import ma.messaging.usermanagementservice.user.register.RegisterRequest;
 
 public interface AccountService {
     boolean register(RegisterRequest request);
+    LoginResponse login(LoginRequest request);
 }
